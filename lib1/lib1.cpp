5683f96d06281ceae2a4b657e6a97312595daf9d
Steve Lorimer <steve@vivcourt.com>
2016-01-26 16:02:41 -0500

    binary output dirs

---
+++ lib1/lib1.cpp
@@ -0,0 +1,6 @@
+#include "lib1.h"
+
+std::string lib1()
+{
+	return __PRETTY_FUNCTION__;
+}
---
+++ lib1/test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "lib1/lib1.h"
+
+int main()
+{
+	std::cout << "this is lib1/test\n";
+	std::cout << "the result of lib1() is " << lib1() << "\n";
+
+	return 0;
+}
