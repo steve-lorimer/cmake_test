81e5418c8d3c7e9d95330ab9e8bdcb316317e58f
Steve Lorimer <steve@vivcourt.com>
2016-04-20 10:48:05 -0500

    version info

---
+++ scripts/cmake/string.cmake
@@ -0,0 +1,22 @@
+function(word_count INPUT_STRING NUM_WORDS)
+    
+    if (INPUT_STRING STREQUAL "")
+        
+        # empty string = 0 words
+        set (len 0)
+
+    else()
+
+        # convert string into list
+        string(REPLACE " "  ";" input_list ${INPUT_STRING})
+        string(REPLACE "\n" ";" input_list ${input_list})
+
+        # get length of list
+        list  (LENGTH input_list len)
+
+    endif()
+
+    # store result in NUM_WORDS
+    set(${NUM_WORDS} ${len} PARENT_SCOPE)
+
+endfunction()
---
+++ scripts/cmake/version_bak.cmake
@@ -0,0 +1,65 @@
+include(string)
+
+# commit hash, with dirty status, tags
+execute_process(
+    OUTPUT_VARIABLE   VERSION
+    COMMAND           git describe --always --dirty --long --tags
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+# number of commits
+execute_process(
+    OUTPUT_VARIABLE   NUM_COMMITS
+    COMMAND           git rev-list HEAD
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+# branch name
+execute_process(
+    OUTPUT_VARIABLE   BRANCH
+    COMMAND           git rev-parse --abbrev-ref HEAD
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+# number of commits ahead of origin
+execute_process(
+    OUTPUT_VARIABLE   AHEAD_BY
+    COMMAND           git log --oneline origin/${BRANCH}..${BRANCH}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+# number of untracked files
+execute_process(
+    OUTPUT_VARIABLE   NUM_UNTRACKED
+    COMMAND           git ls-files --exclude-standard --others --full-name -- 
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+# hostname
+execute_process(
+    OUTPUT_VARIABLE   HOSTNAME
+    COMMAND           hostname
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+# user
+set(USER $ENV{USER})
+
+# build variant
+string(TOLOWER ${CMAKE_BUILD_TYPE} BUILD_VARIANT)
+
+word_count("${NUM_COMMITS}"   NUM_COMMITS)
+word_count("${AHEAD_BY}"      AHEAD_BY)
+word_count("${NUM_UNTRACKED}" NUM_UNTRACKED)
+
+message(STATUS "branch=${BRANCH} commits=${NUM_COMMITS} ahead_by=${AHEAD_BY} num_untracked=${NUM_UNTRACKED} version=${VERSION}")
+
+configure_file (
+    "${PROJECT_SOURCE_DIR}/app/version_details.h.in"
+    "${PROJECT_SOURCE_DIR}/app/version_details.h"
+    )
