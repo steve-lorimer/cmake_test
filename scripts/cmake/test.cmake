54c8894ee3bf2a90648325a42e18aa26e71b0bfb
Steve Lorimer <steve@vivcourt.com>
2016-04-21 15:37:01 -0500

    calgrind: remove track children

--- scripts/cmake/test.cmake
+++ scripts/cmake/test.cmake
@@ -8,7 +8,7 @@ find_package(Boost COMPONENTS unit_test_framework REQUIRED)
 
 # make tests run through valgrind
 set(CMAKE_MEMORYCHECK_COMMAND         "/usr/bin/valgrind")
-set(CMAKE_MEMORYCHECK_COMMAND_OPTIONS "--trace-children=yes --leak-check=full --track-origins=yes --error-exitcode=1 --quiet")
+set(CMAKE_MEMORYCHECK_COMMAND_OPTIONS "--leak-check=full --track-origins=yes --error-exitcode=1 --quiet")
 set(VALGRIND_CMD                      "${CMAKE_MEMORYCHECK_COMMAND} ${CMAKE_MEMORYCHECK_COMMAND_OPTIONS}")
 separate_arguments(VALGRIND_CMD)
 
