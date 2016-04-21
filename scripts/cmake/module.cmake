ecb46457aa783ccfcce78106b17960971070b662
Steve Lorimer <steve@vivcourt.com>
2016-04-21 11:57:57 -0500

    create_module renamed to module

--- scripts/cmake/module.cmake
+++ scripts/cmake/module.cmake
@@ -1,6 +1,6 @@
 include_guard(__included_module)
 
-function(create_module MODULE)
+function(module MODULE)
  
     # create a phony target MODULE if it doesn't already exist
     if (NOT TARGET ${MODULE})
@@ -13,7 +13,7 @@ endfunction()
 
 function(add_to_module MODULE DEPENDENCY)
  
-    create_module(${MODULE})
+    module(${MODULE})
 
     # add the dependency to be built when MODULE is built
     add_dependencies(${MODULE}
