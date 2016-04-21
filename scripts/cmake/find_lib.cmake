47089f234ae3170ff6264cbf8fb9780db9a719fd
Steve Lorimer <steve@vivcourt.com>
2016-04-21 15:12:53 -0500

    find library needs to use a unique variable name so as not to defeat cmakes caching

--- scripts/cmake/find_lib.cmake
+++ scripts/cmake/find_lib.cmake
@@ -3,15 +3,14 @@ include_guard(__included_find_lib)
 function(do_find_lib LIB_NAME SUFFIX OUT)
 
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${SUFFIX})
-    set(FOUND FOUND-NOTFOUND)
 
-    find_library(FOUND ${LIB_NAME})
+    find_library(FOUND_${LIB_NAME}${SUFFIX} ${LIB_NAME})
 
-    if(NOT FOUND)
+    if(NOT FOUND_${LIB_NAME}${SUFFIX})
         message(SEND_ERROR "unable to find library ${LIB_NAME}")
     endif()
 
-    set(${OUT} ${FOUND} PARENT_SCOPE)
+    set(${OUT} ${FOUND_${LIB_NAME}${SUFFIX}} PARENT_SCOPE)
 
 endfunction()
 
